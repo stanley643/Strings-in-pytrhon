f6901bf73e21547ea9047d4177f69c0df3076c4c
Stanley Murigi Njoroge <98167574+stanley643@users.noreply.github.com>
2022-04-14 18:50:48 +0300

    Add files via upload

---
+++ interpolation.py
@@ -0,0 +1,12 @@
+print("{} is {} years old!".format("Stan", 22))
+
+output = "{0} is {1} years old, and {0} works as a {2}."
+print(output.format("Stanley", 24, "web developer"))
+
+output2 = "{name} is {age} years old, and {name} works as a {job}."
+print(output2.format(name="Stan", age=24, job="web developer"))
+
+
+
+
+
---
+++ stringProcessing.py
@@ -0,0 +1,15 @@
+a= "hello, world".lower()
+b = "hello, world".upper()
+c = "hello, woRLd".capitalize()
+d =  "hello, world".title()
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+e= "  hello, world  ".strip().upper() #removes the white space at the end
+print(e)
+
+
+
